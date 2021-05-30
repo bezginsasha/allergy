1f83d794aefcee155dfc37e37633b8407cf0ac08
mrtedn <bezgin.sasha06@gmail.com>
2021-05-30 21:19:39 +0700

    raise 404 when not found allergy in detail view

--- alg/views.py
+++ alg/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Allergy
@@ -12,5 +12,5 @@ def index(request):
 	return render(request, 'alg/index.html', context)
 
 def detail(request, allergy_id):
-	a = Allergy.objects.get(id=allergy_id)
+	a = get_object_or_404(Allergy, id=allergy_id)
 	return HttpResponse(str(a))
