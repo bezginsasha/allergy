fae8b251cdfb01065688b27c745bd29b7c3806d9
mrtedn <bezgin.sasha06@gmail.com>
2021-05-30 21:27:21 +0700

    in index view add displaying of all allergies in string

--- alg/views.py
+++ alg/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Allergy
+
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello world")
+	allergy_list = Allergy.objects.all()
+	output = ', '.join([a.name for a in allergy_list])
+	return HttpResponse(output)
