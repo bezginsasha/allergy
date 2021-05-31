05fb1a933ea02abb3734cd885df8cc145b60312c
mrtedn <bezgin.sasha06@gmail.com>
2021-05-31 20:45:47 +0700

    add namespace to urls

--- alg/urls.py
+++ alg/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .  import views
 
+app_name = 'alg'
+
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:allergy_id>', views.detail, name='detail'),
