15b001ae3998102cbaed697359dc24cc6c6ee4bb
mrtedn <bezgin.sasha06@gmail.com>
2021-06-05 01:22:50 +0700

    add hiding and showing of buttons on more-button click

--- alg/static/alg/main.js
+++ alg/static/alg/main.js
@@ -1,1 +1,37 @@
-//alert('Hello world!');
+var lastId;
+
+function getElementById(id) {
+	return document.querySelector('#' + id);
+}
+
+function hideElement(element) {
+	element.style.display = 'none';
+}
+
+function showElement(element) {
+	element.style.display = 'inline-block';
+}
+
+function showEditButtons(allergyId) {
+	showElement(getElementById('delete' + allergyId));
+	showElement(getElementById('edit' + allergyId));
+
+	hideElement(getElementById('enrage' + allergyId));
+	hideElement(getElementById('more' + allergyId));
+}
+
+function showEnrageButton(allergyId) {
+	hideElement(getElementById('delete' + allergyId));
+	hideElement(getElementById('edit' + allergyId));
+
+	showElement(getElementById('enrage' + allergyId));
+	showElement(getElementById('more' + allergyId));
+}
+
+function show(allergyId) {
+	if (lastId != undefined) {
+		showEnrageButton(lastId);
+	}
+	showEditButtons(allergyId);
+	lastId = allergyId;
+}
