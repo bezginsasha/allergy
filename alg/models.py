2e6dae5daf675e0769ebfae8c11c0849b9abea71
mrtedn <bezgin.sasha06@gmail.com>
2021-06-20 10:34:15 +0700

    method of allergy model for saving with handle of unique constraint

--- alg/models.py
+++ alg/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.db import IntegrityError
 
 # Create your models here.
 
 class Allergy(models.Model):
 	name = models.CharField(max_length=100, unique=True)
 	anger = models.IntegerField(default=0)
+
+	def save_with_unique_handle(self):
+		if len(self.name) < 1:
+			return
+
+		try:
+			self.save()
+		except IntegrityError as e:
+			pass
 
 	def __str__(self):
 		return "allergy " + self.name + " with anger: " + str(self.anger)
