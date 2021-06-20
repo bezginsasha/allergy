a197bec62bf5dcd1d08c070bb1444c04652013c5
mrtedn <bezgin.sasha06@gmail.com>
2021-06-20 10:32:58 +0700

    add unique constraint for name in allergy model

--- alg/models.py
+++ alg/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Allergy(models.Model):
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=100, unique=True)
 	anger = models.IntegerField(default=0)
 
 	def __str__(self):
